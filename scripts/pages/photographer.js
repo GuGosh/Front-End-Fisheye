ced155df5ece86e5815d5397c953f33467bc77a0
Hugo Olekhnovitch <hugo@olek.fr>
2022-02-28 17:03:26 -0500

    intégration modal contact

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -7,6 +7,8 @@ async function init() {
     const photographer = await Api.getPhotographer(idPhotographer)
     const userCardDOM = photographer.getSinglePhotograherDom();
     Photographer.singlePhotographerSection.insertAdjacentHTML('beforeend', userCardDOM);
+    const photographer_name_contact = document.querySelector('#contact_modal .photographer-name');
+    photographer_name_contact.innerHTML = photographer.name;
 
     return photographer
 };
@@ -31,6 +33,21 @@ function addLike(domElement) {
     globalLikes.innerHTML = parseInt(globalLikes.innerHTML) + 1;
 }
 
+const modal = document.getElementById('contact_modal');
+const buttonOpenModal = document.querySelectorAll('body .open-modal');
+const buttonCloseModal = document.querySelectorAll('body .close-modal');
+
+buttonOpenModal.forEach((element) => element.addEventListener('click', displayModal));
+
+buttonCloseModal.forEach((element) => element.addEventListener('click', closeModal));
+
+function displayModal() {
+    modal.style.display = 'flex';
+}
+
+function closeModal() {
+    modal.style.display = 'none';
+}
 
 
 
