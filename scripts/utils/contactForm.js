06fb2e4cea078bb30e37b6baffdc0d6c983ba144
Hugo Olekhnovitch <hugo@olek.fr>
2022-03-23 15:32:25 -0400

    log values contact form on submit

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -5,6 +5,7 @@ export function initModalListener() {
     const modal = document.getElementById('contact_modal');
     const buttonOpenModal = document.querySelectorAll('body .open-modal');
     const buttonCloseModal = document.querySelectorAll('body .close-modal');
+    const contactForm = document.querySelector('#contact_modal form');
 
     buttonOpenModal.forEach((element) => element.addEventListener('click', () => {
         displayModal(modal);
@@ -13,4 +14,11 @@ export function initModalListener() {
     buttonCloseModal.forEach((element) => element.addEventListener('click', () => {
         closeModal(modal);
     }));
+
+    contactForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const fieldsForm = contactForm.elements;
+        const valuesForm = { 'firstname': fieldsForm['firstname'].value, 'name': fieldsForm['name'].value, 'email': fieldsForm['email'].value, 'message': fieldsForm['message'].value }
+        console.log(valuesForm);
+    })
 }
